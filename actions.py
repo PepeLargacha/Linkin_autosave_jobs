359429072acc43d932e8e4cb78334efb6269ee6f
Pedro Largacha <86388846+PepeLargacha@users.noreply.github.com>
2022-06-02 15:36:35 -0300

    first commit

---
+++ actions.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException as Stale
+from selenium.common.exceptions import NoSuchElementException as NoSuch
+from selenium.webdriver.common.by import By
+
+
+def click_job(counter: int, webd: webdriver):
+    try:
+        WebDriverWait(webd, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, f'//section[1]/div/div/ul/li[{counter}]')))
+
+        webd.find_element(By.XPATH,
+                        f'//section[1]/div/div/ul/li[{counter}]').click()
+
+    except (Stale, NoSuch):
+        webd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        WebDriverWait(webd, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, f'//section[1]/div/div/ul/li[{counter}]')))
+
+        webd.find_element(By.XPATH,
+                        f'//section[1]/div/div/ul/li[{counter}]').click()
+
+
+def click_save_job(webd: webdriver):
+
+    try:
+        WebDriverWait(webd, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//div[@class=\
+"jobs-unified-top-card__content--two-pane"]//button/span[text()="Save"]')))
+
+        webd.find_element(
+            By.XPATH, '//div[@class="jobs-unified-top-card__content--two-pane"]//\
+button/span[text()="Save"]').click()
+
+    except (Stale, NoSuch):
+        WebDriverWait(webd, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//div[@class=\
+"jobs-unified-top-card__content--two-pane"]//button/span[text()="Save"]')))
+
+        webd.find_element(
+            By.XPATH, '//div[@class="jobs-unified-top-card__content--two-pane"]//\
+button/span[text()="Save"]').click()
+
+    finally:
+        webd.find_element(
+                By.XPATH, 
+                '//div[@class="artdeco-toast-item artdeco-toast-item--visible \
+ember-view"]/*/li-icon[@type="cancel-icon"]').click()
---
+++ main.py
@@ -0,0 +1,40 @@
+# Main
+from os import environ
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from actions import click_save_job
+
+CHROMIUM_PATH = 'C:/Programs/chromedriver/chromedriver'
+EMAIL = environ.get('EMAIL')
+PASSWORD = environ.get('LINKEDIN_PASSWORD')
+URL = 'https://www.linkedin.com/'
+JOBS_TO_SAVE = 10
+
+# set up browser
+wd = webdriver.Chrome(CHROMIUM_PATH)
+wd.implicitly_wait(5)
+wd.set_window_size(1200, 1024)
+
+# open linkedin
+wd.get(URL)
+
+# login
+WebDriverWait(wd, 10).until(
+    EC.presence_of_element_located((By.ID, 'session_key')))
+wd.find_element('id', 'session_key').send_keys(EMAIL)
+wd.find_element('id', 'session_password').send_keys(PASSWORD)
+wd.find_element(By.CSS_SELECTOR, 'button.sign-in-form__submit-button').click()
+
+# go to jobs search filtered page (if you want other filters just select them
+# and copy the url)
+wd.get(URL + 'jobs/search/?f_E=2%2C3%2C4&f_JT=F%2CP%2CC%2CT&f_TPR=r604800&f\
+_WT=2&geoId=106057199&keywords=product%20manager&location=Worldwide&sortBy=DD')
+
+# loop through n jobs
+counter = 1
+for n in range(JOBS_TO_SAVE):
+    wd.find_element(By.XPATH, f"//section[1]/div/div/ul/li[{counter}]").click()
+    counter += 1
+    click_save_job(wd)
